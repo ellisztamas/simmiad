46f503ebf4854e7d85aa619623683b84dba40d4b
Tom Ellis <thomas.ellis@gmi.oeaw.ac.at>
2023-05-16 09:34:19 +0200

    Added habitat_clustering (forgotten in last commit)
     Changes to be committed:
    	new file:   R/habitat_clustering.R
    	new file:   tests/testthat/test_habitat_clustering.R

---
+++ R/habitat_clustering.R
@@ -0,0 +1,26 @@
+#' Do genotypes cluster by microhabitat?
+#'
+#' Calculates how many pairs of plants of the same genotype are also in the
+#' same microhabitat.
+#'
+#' @param genotype Vector of genotype labels.
+#' @param habitat Vector of habitat labels of the same length as `genotype`.
+#'
+#' @return Proportion of pairs of individuals in the same habitat that are
+#' identical.
+#' @author Tom Ellis
+habitat_clustering <- function(genotype, habitat){
+  if(length(genotype) != length(habitat)){
+    stop("`genotype` and `habitat` are different lengths.")
+  }
+  # For all pairs of plants, check whether genotypes are the same
+  genotype_match <- combn(genotype, 2)
+  genotype_match <- genotype_match[1,] == genotype_match[2,]
+  # For all pairs of plants, check whether habitats are the same
+  habitat_match <- combn(habitat, 2)
+  habitat_match <- habitat_match[1,] == habitat_match[2,]
+  # Check whether genotype and habitat match.
+  both_match <- genotype_match * habitat_match
+
+  sum(both_match, na.rm = TRUE) / sum(habitat_match, na.rm = TRUE)
+}
---
+++ tests/testthat/test_habitat_clustering.R
@@ -0,0 +1,15 @@
+test_that(
+  "habitat_clustering gives expected values", {
+  # No concordance between habitat and genotype
+  t1 <- habitat_clustering(
+    genotype = paste0("g", 1:12),
+    habitat = rep(paste0('h', LETTERS[1:3]), each = 4)
+  )
+  expect_equal(t1 , 0)
+  # Perfect concordance between habitat and genotype
+  t2 <- habitat_clustering(
+    genotype = rep(paste0('g', LETTERS[1:3]), each =  4),
+    habitat = rep(paste0('h', LETTERS[1:3]), each = 4)
+  )
+  expect_equal(t2, 1)
+  })
